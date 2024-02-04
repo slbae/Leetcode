b8e8a0b46aa5a816a70f229694c50125c1596a86
Sylbi Bae <slbae@ncsu.edu>
2024-02-04 02:07:02 -0500

    Skeleton

---
+++ easy/test/PlusOneTest.java
@@ -0,0 +1,38 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+public class PlusOneTest {
+
+    @Test
+    public void test123() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void test4321() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void test9() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void test2475395734() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void test9090() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testLen100() {
+        fail("Not yet implemented");
+    }
+}
